4a9e462bdedf74aec33cdb98fa15e8ceca7c8e6b
112ntumesa-dotcom <112ntumesa@gmail.com>
2026-09-03 00:11:25 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * NTUMESA 網站設定檔 (方案一：Google Sheet CSV 直連)
+ * 請將以下網址替換為你在「發布到網路」中取得的 CSV 連結
+ */
+const CSV_CONFIG = {
+  // 活動公告工作表 CSV 網址
+  announcementsUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTx0GJyuaRNXxtHNiN750VXs5IOkjA1hdOKsEEPVB7UluLglOm_F-vsix4cMoej9egFkAAdkLQfuVTS/pub?gid=1237467680&single=true&output=csv",
+  
+  // 成員名單工作表 CSV 網址
+  membersUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTx0GJyuaRNXxtHNiN750VXs5IOkjA1hdOKsEEPVB7UluLglOm_F-vsix4cMoej9egFkAAdkLQfuVTS/pub?gid=597949381&single=true&output=csv",
+  
+  // 檔案清單工作表 CSV 網址
+  filesUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTx0GJyuaRNXxtHNiN750VXs5IOkjA1hdOKsEEPVB7UluLglOm_F-vsix4cMoej9egFkAAdkLQfuVTS/pub?gid=1397357920&single=true&output=csv"
+};
